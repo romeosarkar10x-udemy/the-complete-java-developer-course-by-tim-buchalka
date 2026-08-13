f11751d5773dd74294c45401d96d700f2a245ca7
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-08-13 22:05:52 +0530

    Completed lectures

---
+++ first_steps/ExploringBoolean.java
@@ -0,0 +1,6 @@
+public class ExploringBoolean {
+    public static void main(String[] args) {
+        boolean myBool = false;
+        System.out.println("myBool: " + myBool);
+    }
+}
---
+++ first_steps/IntroToStrings.java
@@ -0,0 +1,7 @@
+public class IntroToStrings {
+    public static void main(String[] args) {
+        String myString = "This is a string";
+        System.out.println("'myString' is equal to '" + myString + "'");
+        System.out.println("I wish I had \u00241,000,000,000");
+    }
+}
